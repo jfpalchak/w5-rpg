a331c77208a858704756c9579ed5e5e1dc9a51ce
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 15:01:27 -0700

    Add tests for Weapon and Armor.

--- __tests__/item.test.js
+++ __tests__/item.test.js
@@ -1,4 +1,4 @@
-import {Item} from './../src/js/item.js';
+import {Item, Weapon} from './../src/js/item.js';
 
 describe('Item', () => {
 
@@ -7,5 +7,30 @@ describe('Item', () => {
     expect(newItem).toEqual({item: "weapon"});
   });
 
-  
+});
+
+describe('Weapon', () => {
+
+  test('should create a Weapon object, as an extension of Item', () => {
+    let newWeapon = new Weapon("sword", 10);
+    expect(newWeapon).toEqual({
+      item: "weapon",
+      type: "sword",
+      attack: 10
+    });
+  });
+
+});
+
+describe('Armor', () => {
+
+  test('should create an Armor object, as an extension of Item', () => {
+    let newWeapon = new Weapon("shield", 15);
+    expect(newWeapon).toEqual({
+      item: "armor",
+      type: "shield",
+      defense: 15
+    });
+  });
+
 });
