8805fe206445e5b61d09a19b465d10c340ed0b02
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 14:54:41 -0700

    Create new class Item, pass test 1.

---
+++ src/js/item.js
@@ -0,0 +1,21 @@
+export class Item {
+  constructor(item) {
+    this.item = item;
+  }
+}
+
+export class Weapon extends Item {
+  // constructor(type, attackPower) {
+    // super("weapon");
+    // this.type = type;
+    // this.attack = attackPower;
+  // }
+}
+
+export class Armor extends Item {
+  // constructor(type, defensePower) {
+  //   super("armor");
+  //   this.type = type;
+  //   this.defense = defensePower;
+  // }
+}
