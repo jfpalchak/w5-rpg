2b25933d1fada285a675cf0b9b15e0910a8e70c3
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 10:11:07 -0700

    Add test 2 for takeDamage.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -4,9 +4,11 @@ describe('Character', () => {
 
   let character;
   let swordItem;
+  let shieldItem;
   beforeEach(() => {
     character = new Character(3, 100);
     swordItem = {item: "weapon", type: "sword", attack: 10};
+    shieldItem = {item: "armor", type: "shield", defense: 15};
   });
 
   test('should create an undefined character object', () => {
@@ -19,13 +21,7 @@ describe('Character', () => {
     expect(damage).toEqual(3);
   });
 
-  test('should compute damage caused based on character weapon', () => {
-    character.addItem(swordItem);
-    let damage = character.dealDamage();
-    expect(damage).toEqual(30);
-  });
-
-  test('should compute character health value  taken', () => {
+  test('should compute character health value after taking damage', () => {
     character.takeDamage(30);
     expect(character.health).toEqual(70);
   });
@@ -33,6 +29,18 @@ describe('Character', () => {
   test('should add an item to the character inventory', () => {
     character.addItem(swordItem);
     expect(character.inventory).toEqual({weapon: {item: "weapon", type: "sword", attack: 10}});
+  });
+
+  test('should compute damage caused based on character weapon', () => {
+    character.addItem(swordItem);
+    let damage = character.dealDamage();
+    expect(damage).toEqual(30);
+  });
+
+  test('should compute character health value after taking damage, based on character armor', () => {
+    character.addItem(shieldItem);
+    character.takeDamage(30);
+    expect(character.health).toEqual(85);
   });
 
 });
