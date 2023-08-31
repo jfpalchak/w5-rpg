9823c7b4bf072048c235b605b00a91b70c8cadf5
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 09:58:19 -0700

    Add test 2 for dealDamage.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,8 +3,10 @@ import {Character} from './../src/js/character.js';
 describe('Character', () => {
 
   let character;
+  let swordItem;
   beforeEach(() => {
     character = new Character(3, 100);
+    swordItem = {item: "weapon", type: "sword", attack: 10};
   });
 
   test('should create an undefined character object', () => {
@@ -17,13 +19,18 @@ describe('Character', () => {
     expect(damage).toEqual(3);
   });
 
+  test('should compute damage caused based on character weapon', () => {
+    character.addItem(swordItem);
+    let damage = character.dealDamage();
+    expect(damage).toEqual(30);
+  });
+
   test('should compute character health value  taken', () => {
     character.takeDamage(30);
     expect(character.health).toEqual(70);
   });
 
   test('should add an item to the character inventory', () => {
-    const swordItem = {item: "weapon", type: "sword", attack: 10};
     character.addItem(swordItem);
     expect(character.inventory).toEqual({weapon: {item: "weapon", type: "sword", attack: 10}});
   });
