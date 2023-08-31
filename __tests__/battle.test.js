f8272578a678276b546cf1b4f2db69290749a5b2
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 11:34:06 -0700

    Add tests for numberOfPlayers and addPlayer.

--- __tests__/battle.test.js
+++ __tests__/battle.test.js
@@ -3,9 +3,24 @@ import {Character} from './../src/js/character.js';
 
 describe ('Battle', () => {
 
+  let battle;
+  beforeEach( () => {
+    battle = new Battle();
+  });
+
   test('should create a battle object', () => {
-    let battle = new Battle();
     expect(battle.players).toEqual({});
+  });
+
+  test('should return the number of players currently in battle', () => {
+    let number = battle.numberOfPlayers();
+    expect(number).toEqual(0);
+  });
+
+  test('should add character object to list of players in battle', () => {
+    let warrior = new Character(3, 100);
+    battle.addPlayer(warrior);
+    expect(battle.players).toEqual({1: {strength: 3, health: 100, inventory: {}}});
   });
 
 
