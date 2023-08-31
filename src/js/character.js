e55e9d60bd1cb0324a4968827476cb300af1d817
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 10:03:24 -0700

    Pass test 2 for dealDamage.

--- src/js/character.js
+++ src/js/character.js
@@ -5,27 +5,23 @@ export class Character {
     this.inventory = {};
   }
 
+  // calculate damage dealt by character
   dealDamage() {
     let damage = this.strength;
+    if (this.inventory.weapon) {
+      damage = this.strength * this.inventory.weapon.attack;
+    }
     return damage;
-
   }
 
+  // calculate character health based on damage received
   takeDamage(damage) {
     this.health = this.health - damage;
   }
 
+  // add item object to character inventory
   addItem(newItem) {
     this.inventory[newItem.item] = newItem;
   }
-  
 
 }
-
-
-
-
-
-
-// if (has armor) 
-//   damage - armor; // reduce damage
