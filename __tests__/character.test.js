8c4552384baecca65ec14048d0e5309e54b12ad4
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 09:32:08 -0700

    Add test for addItem method.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -7,19 +7,25 @@ describe('Character', () => {
     character = new Character(3, 100);
   });
 
-  test('should create an character object', () => {
+  test('should create an undefined character object', () => {
     let character = new Character();
     expect(character).toEqual({strength: undefined, health: undefined, inventory: {}});
   });
 
-  test('should compute damage caused', () => {
+  test('should compute damage caused based on strength', () => {
     let damage = character.dealDamage();
     expect(damage).toEqual(3);
   });
 
-  test('should compute damage taken', () => {
+  test('should compute character health value  taken', () => {
     character.takeDamage(30);
     expect(character.health).toEqual(70);
+  });
+
+  test('should add an item object to the character inventory', () => {
+    const swordItem = {type: "Sword", attack: 10};
+    character.addItem(swordItem);
+    expect(character.inventory).toEqual({type: "Sword", attack: 10});
   });
 
 });
