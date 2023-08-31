73ee8865f4febfa9a46a187b0ac8d28f474fbe33
Joey Palchak <joey.palchak@gmail.com>
2023-08-31 11:42:57 -0700

    Pass test for assignId.

--- src/js/battle.js
+++ src/js/battle.js
@@ -3,6 +3,12 @@ import { Character } from './character.js';
 export class Battle {
   constructor() {
     this.players = {};
+    this.currentId = 0;
+  }
+
+  assignId() {
+    this.currentId += 1;
+    return this.currentId;
   }
 
   numberOfPlayers() {
